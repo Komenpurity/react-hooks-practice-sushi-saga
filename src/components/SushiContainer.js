61ebbe10a3d261c9256c7314b8b6ca236cad50fe
Komenpurity <jepkorirpurity8@gmail.com>
2023-05-11 11:30:59 +0300

    added display of sushi in 4 sets

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
 import Sushi from "./Sushi";
 
 function SushiContainer({sushi}) { 
+
+  const [sushiNumber,setSushiNumber] = useState(4) 
+
+  const addFourSushi = () => {
+    setSushiNumber(sushiNumber + 4)
+  }
+
   return (
     <div className="belt">
-      {sushi.map( (item) => {
+      {sushi.slice(sushiNumber-4,sushiNumber).map( (item) => {
         return <Sushi id = {item.id} name = {item.name} url = {item.img_url} price = {item.price}/> 
       })}
-      <MoreButton />
+      <MoreButton addFourSushi={addFourSushi}/>
     </div>
   );
 }
